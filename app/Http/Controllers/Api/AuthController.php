301fcfb8a1a8407b6716dd633a78fca154c8202a
carvalholjcc <carvalholjcc@TIAD-512076.dacta1.intraer>
2018-03-08 15:29:15 -0300

    Resposta de erro ao requistar token

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace SON\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use SON\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use SON\Models\Admin;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+
+
+    protected function credentials(Request $request)
+    {
+        $data = $request->only($this->username(), 'password');
+        $usernameKey = $this->usernameKey();
+        $data[$usernameKey] = $data[$this->username()];
+       // $data['userable_type'] = Admin::class;
+        unset($data[$this->username()]);
+        return $data;
+    }
+
+    protected function usernameKey()
+    {
+        $email = \Request::get($this->username());
+        $validator = \Validator::make([
+            'email' => $email
+        ], ['email' => 'email']);
+        return $validator->fails() ? 'enrolment' : 'email';
+    }
+
+    public function username()
+    {
+        return 'username';
+    }
+    /**
+    * Gerando Token
+    */
+    public function accessToken(Request $request)
+    {
+        $this->validateLogin($request);
+        $credentials = $this->credentials($request);
+        if ($token = \Auth::guard('api')->attempt($credentials)) {
+            return ['token' => $token];
+        }
+
+        return response()->json([
+          'error' => \Lang::get('auth.failed')
+        ],400);
+
+    }
+
+    public function logout(){
+        \Auth::guard('api')->logout();
+        return response()->json([],204); //No content
+    }
+}
